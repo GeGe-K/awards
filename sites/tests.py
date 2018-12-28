b9b3033d07f2739ca332f1f9105b65997c820d15
Sam Kasyoki <samkasyoki@yahoo.com>
2018-12-28 21:16:13 +0300

    create tests for each module

--- sites/tests.py
+++ sites/tests.py
@@ -1,43 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project, Profile, Review
 
 # Create your tests here.
-# from django.test import TestCase
-# from django.contrib.auth.models import User
-# from .models import Project,Profile,Vote
+class ProjectTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
 
-# # Create your tests here.
-# class ProjectTestClass(TestCase):
-#     # Set up method
-#     def setUp(self):
-#         self.user = User.objects.create_user(username='testuser', password='12345')
-#         self.profile = Profile(user = self.user)
-#         self.profile.save()
-#         self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',screenshot='path/to/screenshot',user=self.user,profile=self.profile)
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Project))
 
-#     #Testing instance
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.project,Project))
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
 
-# class ProfileTestClass(TestCase):
-#     # Set up method
-#     def setUp(self):
-#         self.user = User.objects.create_user(username='testuser', password='12345')
-#         self.profile = Profile(id=1,profile_photo='path/to/photo',user = self.user,bio='test bio',contacts='test contact')
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
 
-#     #Testing instance
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.profile,Profile))
+class ReviewTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(profile_pic='images/pic', bio='I love cheese', contacts='+254712345678', user = self.user)
+        self.profile.save()
+        self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',user=self.user)
+        self.project.save()
+        self.review = Review(id=1,project=self.project,design=10,usability=10,content=10,average=7,user=self.user)
 
-# class VoteTestClass(TestCase):
-#     # Set up method
-#     def setUp(self):
-#         self.user = User.objects.create_user(username='testuser', password='12345')
-#         self.profile = Profile(user = self.user)
-#         self.profile.save()
-#         self.project = Project(id=1, image = 'path/to/image',title='test',description='test caption',url='path/to/project',screenshot='path/to/screenshot',user=self.user,profile=self.profile)
-#         self.project.save()
-#         self.vote = Vote(id=1,project=self.project,design=10,usability=10,content=10,profile=self.profile)
-
-#     #Testing instance
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.vote,Vote))
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.review,Review))
